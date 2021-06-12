14392f8d13e9ebb4ecdc5d7cea2507e6e61df15a
sana <hs_boucheta@esi.dz>
2021-06-12 13:59:26 +0100

    added get doctor by id

--- app/controllers/medecin.controller.js
+++ app/controllers/medecin.controller.js
@@ -14,6 +14,28 @@ const getAllMedecins = (req, res) => {
           });
 };
 
+const getMedecin = async (req, res) => {
+     const id = req.params.id;
+
+     try {
+          const medecin = await Medecin.findByPk(id)
+          if (medecin) {
+               res.status(200).send(medecin)
+          }
+          else {
+               res.status(404).send({
+                    error: "not_found",
+                    message: 'there is no doctor with id= ' + id
+               })
+          }
+     } catch {
+          res.status(500).send({
+               message: "Erreur lors de la récupération d'un medecin avec l'id=" + id
+          });
+     }
+};
+
 module.exports = {
-     getAllMedecins
+     getAllMedecins,
+     getMedecin
 }
--- app/routes/medecin.route.js
+++ app/routes/medecin.route.js
@@ -2,6 +2,7 @@ const MedecinController = require('../controllers/medecin.controller');
 var router = require("express").Router();
 
 router.get('/', MedecinController.getAllMedecins)
+router.get('/:id', MedecinController.getMedecin)
 
 
 module.exports = router;
